320cfb24e7fe5dd457b786d07157e8aa786007d3
annakitou <anna.kitou@nb.org>
2023-10-26 11:33:19 +0300

    Add project files.

---
+++ GoogleTextToSpeech/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace GoogleTextToSpeech
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			components = new System.ComponentModel.Container();
+			btnRecordVoice = new Button();
+			btnSave = new Button();
+			btnSpeechInfo = new Button();
+			PauseBtn = new Button();
+			bindingSource1 = new BindingSource(components);
+			SoundBar = new ProgressBar();
+			label1 = new Label();
+			InputListCb = new ComboBox();
+			StopBtn = new Button();
+			textBox1 = new TextBox();
+			((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
+			SuspendLayout();
+			// 
+			// btnRecordVoice
+			// 
+			btnRecordVoice.Location = new Point(33, 91);
+			btnRecordVoice.Name = "btnRecordVoice";
+			btnRecordVoice.Size = new Size(112, 34);
+			btnRecordVoice.TabIndex = 0;
+			btnRecordVoice.Text = "Record";
+			btnRecordVoice.UseVisualStyleBackColor = true;
+			btnRecordVoice.Click += btnRecordVoice_Click_1;
+			// 
+			// btnSave
+			// 
+			btnSave.Location = new Point(33, 162);
+			btnSave.Name = "btnSave";
+			btnSave.Size = new Size(112, 34);
+			btnSave.TabIndex = 1;
+			btnSave.Text = "Save";
+			btnSave.UseVisualStyleBackColor = true;
+			btnSave.Click += this.btnSave_Click;
+			// 
+			// btnSpeechInfo
+			// 
+			btnSpeechInfo.Location = new Point(384, 162);
+			btnSpeechInfo.Name = "btnSpeechInfo";
+			btnSpeechInfo.Size = new Size(112, 34);
+			btnSpeechInfo.TabIndex = 2;
+			btnSpeechInfo.Text = "Convert";
+			btnSpeechInfo.UseVisualStyleBackColor = true;
+			// 
+			// PauseBtn
+			// 
+			PauseBtn.Location = new Point(221, 91);
+			PauseBtn.Name = "PauseBtn";
+			PauseBtn.Size = new Size(112, 34);
+			PauseBtn.TabIndex = 3;
+			PauseBtn.Text = "Pause";
+			PauseBtn.UseVisualStyleBackColor = true;
+			PauseBtn.Click += this.PauseBtn_Click;
+			// 
+			// SoundBar
+			// 
+			SoundBar.Location = new Point(3, 217);
+			SoundBar.Margin = new Padding(4);
+			SoundBar.Name = "SoundBar";
+			SoundBar.Size = new Size(595, 36);
+			SoundBar.TabIndex = 5;
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Location = new Point(33, 29);
+			label1.Name = "label1";
+			label1.Size = new Size(123, 25);
+			label1.TabIndex = 6;
+			label1.Text = "SelectInputLbl";
+			// 
+			// InputListCb
+			// 
+			InputListCb.FormattingEnabled = true;
+			InputListCb.Location = new Point(229, 28);
+			InputListCb.Name = "InputListCb";
+			InputListCb.Size = new Size(369, 33);
+			InputListCb.TabIndex = 7;
+			// 
+			// StopBtn
+			// 
+			StopBtn.Location = new Point(221, 162);
+			StopBtn.Name = "StopBtn";
+			StopBtn.Size = new Size(112, 34);
+			StopBtn.TabIndex = 8;
+			StopBtn.Text = "Stop";
+			StopBtn.UseVisualStyleBackColor = true;
+			StopBtn.Click += this.StopBtn_Click;
+			// 
+			// textBox1
+			// 
+			textBox1.Location = new Point(52, 281);
+			textBox1.Multiline = true;
+			textBox1.Name = "textBox1";
+			textBox1.ScrollBars = ScrollBars.Vertical;
+			textBox1.Size = new Size(519, 136);
+			textBox1.TabIndex = 9;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(10F, 25F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(800, 450);
+			Controls.Add(textBox1);
+			Controls.Add(StopBtn);
+			Controls.Add(InputListCb);
+			Controls.Add(label1);
+			Controls.Add(PauseBtn);
+			Controls.Add(btnSpeechInfo);
+			Controls.Add(btnSave);
+			Controls.Add(btnRecordVoice);
+			Controls.Add(SoundBar);
+			Name = "Form1";
+			Text = "Form1";
+			Load += this.Form1_Load;
+			((System.ComponentModel.ISupportInitialize)bindingSource1).EndInit();
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private Button btnRecordVoice;
+		private Button btnSave;
+		private Button btnSpeechInfo;
+		private Button PauseBtn;
+		private BindingSource bindingSource1;
+		private ProgressBar SoundBar;
+		private Label label1;
+		private ComboBox InputListCb;
+		private Button StopBtn;
+		private TextBox textBox1;
+	}
+}
---
+++ GoogleTextToSpeech/Form1.cs
@@ -0,0 +1,231 @@
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Speech.V1;
+using GoogleTextToSpeech.Entities;
+using Grpc.Auth;
+using NAudio.Utils;
+using NAudio.Wave;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+
+namespace GoogleTextToSpeech
+{
+	public partial class Form1 : Form
+	{
+
+		private BufferedWaveProvider bwp;
+
+		int AudioEndPointId;
+
+		WaveInEvent waveIn;
+		WaveOut waveOut;
+		WaveFileWriter writer;
+		WaveFileReader reader;
+		private MemoryStream audioStream = new MemoryStream();
+		string output = "audio.raw";
+
+		public Form1()
+		{
+			InitializeComponent();
+			EnumerateAudioEndPoints();
+		}
+
+
+		private void InputListCb_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			var SelectedItemSerialized = JsonConvert.SerializeObject(InputListCb.SelectedItem);
+			var SelectedItemDeserialized = JsonConvert.DeserializeObject<ComboboxValue>(SelectedItemSerialized);
+			AudioEndPointId = SelectedItemDeserialized.Id;
+		}
+		void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
+		{
+			Int16[] values = new Int16[e.Buffer.Length / 2];
+			Buffer.BlockCopy(e.Buffer, 0, values, 0, e.Buffer.Length);
+
+			// determine the highest value as a fraction of the maximum possible value
+			float fraction = (float)values.Max() / 32768;
+
+			// Show Sound Level
+			SoundBar.Invoke((MethodInvoker)delegate { SoundBar.Value = (int)(fraction * 70); });
+
+			bwp.AddSamples(e.Buffer, 0, e.BytesRecorded);
+		
+		}
+
+		private void btnRecordVoice_Click_1(object sender, EventArgs e)
+		{
+			waveIn = new NAudio.Wave.WaveInEvent
+			{
+				DeviceNumber = AudioEndPointId, // indicates which microphone to use
+				WaveFormat = new NAudio.Wave.WaveFormat(rate: 44100, bits: 16, channels: 1),
+				BufferMilliseconds = 20
+			};
+
+			waveIn.DataAvailable += WaveIn_DataAvailable;
+			waveIn.StartRecording();
+			bwp = new BufferedWaveProvider(waveIn.WaveFormat);
+			bwp.DiscardOnBufferOverflow = true;
+
+			btnRecordVoice.Enabled = false;
+			btnSave.Enabled = true;
+			btnSpeechInfo.Enabled = false;
+
+			//waveOut = new WaveOut();
+			//waveOut.Init(bwp);
+			//////waveOut.PlaybackStopped += new EventHandler<StoppedEventArgs>(waveOut_PlaybackStopped);
+			//waveOut.Play();
+			waveOut = new WaveOut();
+			waveOut.Init(bwp);
+		}
+
+
+		private void waveOut_PlaybackStopped(object sender, StoppedEventArgs e)
+		{
+
+			waveOut.Stop();
+			reader.Close();
+			reader = null;
+		}
+		private void PauseBtn_Click(object sender, EventArgs e)
+		{
+			waveIn.DataAvailable -= WaveIn_DataAvailable;
+			SoundBar.Invoke((MethodInvoker)delegate { SoundBar.Value = 0; });
+		}
+
+		private void EnumerateAudioEndPoints()
+		{
+			for (int i = -1; i < NAudio.Wave.WaveIn.DeviceCount; i++)
+			{
+				var caps = NAudio.Wave.WaveIn.GetCapabilities(i);
+				InputListCb.Items.Add(new ComboboxValue(i, i.ToString() + " : " + caps.ProductName));
+			}
+		}
+
+		private void StopBtn_Click(object sender, EventArgs e)
+		{
+			if (waveIn != null)
+			{
+				waveIn.StopRecording();
+				waveIn.Dispose();
+				waveIn = null;
+			}
+			if (this.writer == null)
+			{
+				return;
+			}
+
+			SoundBar.Invoke((MethodInvoker)delegate { SoundBar.Value = 0; });
+		}
+
+		private void btnSave_Click(object sender, EventArgs e)
+		{
+			waveIn.StopRecording();
+
+			if (File.Exists("audio.raw"))
+				File.Delete("audio.raw");
+
+			writer = new WaveFileWriter(output, waveIn.WaveFormat);
+
+			btnRecordVoice.Enabled = false;
+			btnSave.Enabled = false;
+			btnSpeechInfo.Enabled = true;
+
+			byte[] buffer = new byte[bwp.BufferLength];
+			int offset = 0;
+			int count = bwp.BufferLength;
+
+			var read = bwp.Read(buffer, offset, count);
+			if (count > 0)
+			{
+				writer.Write(buffer, offset, read);
+			}
+
+			waveIn.Dispose();
+			waveIn = null;
+			writer.Close();
+			writer = null;
+
+			reader = new WaveFileReader("audio.raw"); // (new MemoryStream(bytes));
+			waveOut.Init(reader);
+			//waveOut.PlaybackStopped += new EventHandler<StoppedEventArgs>(waveOut_PlaybackStopped);
+			waveOut.Play();
+			SpeechTransform();
+
+		}
+
+
+		private void SpeechTransform()
+		{
+
+			btnRecordVoice.Enabled = true;
+			btnSave.Enabled = false;
+			btnSpeechInfo.Enabled = false;
+
+			if (File.Exists("audio.raw"))
+			{
+
+				GoogleCredential credentials;
+				using (var stream = new FileStream("C:\\Users\\akitou\\Desktop\\lyrical-marker-402608-ec3896f82d1b.json", FileMode.Open, FileAccess.Read))
+				{
+					credentials = GoogleCredential.FromStream(stream);
+				}
+
+				var channel = new Grpc.Core.Channel(SpeechClient.DefaultEndpoint.ToString(), credentials.ToChannelCredentials());
+
+				var speech = CreateSpeechClient(credentials);
+
+
+				var response = speech.Recognize(new RecognitionConfig()
+				{
+					Encoding = RecognitionConfig.Types.AudioEncoding.Linear16,
+					//  SampleRateHertz = 16000,
+					LanguageCode = "el-GR",
+				}, RecognitionAudio.FromFile("audio.raw"));
+
+
+				textBox1.Text = "";
+
+				foreach (var result in response.Results)
+				{
+					foreach (var alternative in result.Alternatives)
+					{
+						textBox1.Text = textBox1.Text + " " + alternative.Transcript;
+					}
+				}
+
+				if (textBox1.Text.Length == 0)
+					textBox1.Text = "No Data ";
+
+			}
+			else
+			{
+
+				textBox1.Text = "Audio File Missing ";
+
+			}
+
+
+		}
+		public SpeechClient CreateSpeechClient(GoogleCredential credentials)
+		{
+			try
+			{
+				var speech = new SpeechClientBuilder
+				{
+					ChannelCredentials = credentials.ToChannelCredentials(),
+				}.Build();
+				return speech;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Error creating SpeechClient: {ex.Message}");
+				return null;
+			}
+		}
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+	}
+}
